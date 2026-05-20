16b18c7914681e9ea455bba0e64448e778ff88f8
RastaRooster808 <bankspham5@gmail.com>
2026-05-19 20:55:01 -1000

    Create service-worker.js for PWA

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'local-grindz-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/styles/main.css',
+    '/scripts/app.js',
+    '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
